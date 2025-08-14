12c77482e031ed0ab56c7b7174ba126f3f58cd0d
DiegoBrgs <diegosntborges@gmail.com>
2025-08-14 14:27:10 -0300

    máscara para cnpj
    adicionado uma máscara para o cnpj da empresa

--- header.php
+++ header.php
@@ -23,7 +23,8 @@
         <script>
             $(document).ready(function(){
                 $("#telefoneUsuario").mask("(00) 00000-0000");
-                 $("#cpfUsuario").mask("000.000.000-00");
+                $("#cnpj").mask("00.000.000/0000-00");
+                $("#cpfUsuario").mask("000.000.000-00");
                 //$("#cepUsuario").mask("00000-000");
                 //$("#cpfUsuario").mask("000.000.000-00");
             });

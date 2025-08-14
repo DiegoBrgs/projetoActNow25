f8500b58d3ed01e80cb7f2b263f00542855ba998
DiegoBrgs <diegosntborges@gmail.com>
2025-08-14 16:45:04 -0300

    bolo

--- actionProduto.php
+++ actionProduto.php
@@ -8,7 +8,7 @@
             //Verifica o método de requisição do servidor
             if($_SERVER["REQUEST_METHOD"] == "POST"){
                 //Bloco para declaração de variáveis
-                $fotoProjeto = $nomeProjeto = $descricao = $carga_horaria = $vagas = "";
+                $fotoProjeto = $nomeProjeto = $descricao = $carga_horaria = $vagas = $cnpj = "";
 
                 //Variável booleana para controle de erros de preenchimento
                 $erroPreenchimento = false;
@@ -53,7 +53,7 @@
                 }
                 else{
                     //Armazena valor do formulário na variável
-                    $carga_horaria = filtrar_entrada($_POST["carga_horaria"]);
+                    $cnpj = filtrar_entrada($_POST["cnpj"]);
                 }
 
                 //Validação do campo vagas
@@ -102,7 +102,7 @@
                 if(!$erroPreenchimento && !$erroUpload){
 
                     //Cria uma variável para armazenar a QUERY para realizar a inserção dos dados do produto na tabela Projetos
-                    $inserirProjeto = "INSERT INTO Projeto (fotoProjeto, nomeProjeto, descricao, carga_horaria, status_projeto, vagas, cnpj) VALUES ('$fotoProjeto', '$nomeProjeto', '$descricao', '$carga_horaria', 'disponivel', '$vagas', '$cnpj')";
+                    $inserirProjeto = "INSERT INTO Projeto (fotoProjeto, nomeProjeto, descricao, carga_horaria, status_projeto, vagas, cnpj) VALUES ('$fotoProjeto', '$nomeProjeto', '$descricao', '$carga_horaria', 'disponivel', '$vagas', $cnpj)";
 
                     //Inclui o arquivo de conexão com o Banco de Dados
                     include("conexaoBD.php");

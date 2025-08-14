eb038728580691e97cf47dac98b361a9e4395bcd
alon <camiloalon07@gmail.com>
2025-08-14 14:22:13 -0300

    Produtos para projetos
    Trocados o nome das variáveis "...produtos" para de "..projeto" no form e action

--- actionProduto.php
+++ actionProduto.php
@@ -8,55 +8,55 @@
             //Verifica o método de requisição do servidor
             if($_SERVER["REQUEST_METHOD"] == "POST"){
                 //Bloco para declaração de variáveis
-                $fotoProduto = $nomeProduto = $descricaoProduto = $valorProduto = "";
+                $fotoProjeto = $nomeProjeto = $descricaoProjeto = $valorProjeto = "";
 
                 //Variável booleana para controle de erros de preenchimento
                 $erroPreenchimento = false;
 
-                //Validação do campo nomeProduto
+                //Validação do campo nomeProjeto
                 //Utiliza a função empty() para verificar se o campo está vazio
-                if(empty($_POST["nomeProduto"])){
+                if(empty($_POST["nomeProjeto"])){
                     echo "<div class='alert alert-warning text-center'>O campo <strong>NOME</strong> é obrigatório!</div>";
                     $erroPreenchimento = true;
                 }
                 else{
                     //Armazena valor do formulário na variável
-                    $nomeProduto = filtrar_entrada($_POST["nomeProduto"]);
+                    $nomeProjeto = filtrar_entrada($_POST["nomeProjeto"]);
                 }
 
-                //Validação do campo descricaoProduto
+                //Validação do campo descricaoProjeto
                 //Utiliza a função empty() para verificar se o campo está vazio
-                if(empty($_POST["descricaoProduto"])){
+                if(empty($_POST["descricaoProjeto"])){
                     echo "<div class='alert alert-warning text-center'>O campo <strong>DESCRIÇÃO</strong> é obrigatório!</div>";
                     $erroPreenchimento = true;
                 }
                 else{
                     //Armazena valor do formulário na variável
-                    $descricaoProduto = filtrar_entrada($_POST["descricaoProduto"]);
+                    $descricaoProjeto = filtrar_entrada($_POST["descricaoProjeto"]);
                 }
 
-                //Validação do campo valorProduto
+                //Validação do campo valorProjeto
                 //Utiliza a função empty() para verificar se o campo está vazio
-                if(empty($_POST["valorProduto"])){
+                if(empty($_POST["valorProjeto"])){
                     echo "<div class='alert alert-warning text-center'>O campo <strong>VALOR</strong> é obrigatório!</div>";
                     $erroPreenchimento = true;
                 }
                 else{
                     //Armazena valor do formulário na variável
-                    $valorProduto = filtrar_entrada($_POST["valorProduto"]);
+                    $valorProjeto = filtrar_entrada($_POST["valorProjeto"]);
                 }
 
                 //Início da validação da foto do produto
                 $diretorio    = "img/"; //Define para qual diretório as imagens serão movidas
-                $fotoProduto  = $diretorio . basename($_FILES['fotoProduto']['name']); //img/joaozinho.jpg
-                $tipoDaImagem = strtolower(pathinfo($fotoProduto, PATHINFO_EXTENSION)); //Pega o tipo do arquivo em letras minúsculas
+                $fotoProjeto  = $diretorio . basename($_FILES['fotoProjeto']['name']); //img/joaozinho.jpg
+                $tipoDaImagem = strtolower(pathinfo($fotoProjeto, PATHINFO_EXTENSION)); //Pega o tipo do arquivo em letras minúsculas
                 $erroUpload   = false; //Variável para controle do upload da foto
 
                 //Verifica se o tamanho do arquivo é DIFERENTE DE ZERO
-                if($_FILES['fotoProduto']['size'] != 0){
+                if($_FILES['fotoProjeto']['size'] != 0){
 
                     //Verifica se o tamanho do arquivo é maior do que 5 MegaBytes (MB) - Medida em Bytes
-                    if($_FILES['fotoProduto']['size'] > 5000000){
+                    if($_FILES['fotoProjeto']['size'] > 5000000){
                         echo "<div class='alert alert-warning text-center'>A <strong>FOTO</strong> deve ter tamanho máximo de 5MB!</div>";
                         $erroUpload = true;
                     }
@@ -68,7 +68,7 @@
                     }
 
                     //Verifica se a imagem foi movida para o diretório IMG, utilizando a função move_uploaded_file
-                    if(!move_uploaded_file($_FILES['fotoProduto']['tmp_name'], $fotoProduto)){
+                    if(!move_uploaded_file($_FILES['fotoProjeto']['tmp_name'], $fotoProjeto)){
                         echo "<div class='alert alert-danger text-center'>Erro ao tentar mover a <strong>FOTO</strong> para o diretório $diretorio!</div>";
                         $erroUpload = true;
                     }
@@ -83,7 +83,7 @@
                 if(!$erroPreenchimento && !$erroUpload){
 
                     //Cria uma variável para armazenar a QUERY para realizar a inserção dos dados do produto na tabela Produtos
-                    $inserirProduto = "INSERT INTO Produtos (fotoProduto, nomeProduto, descricaoProduto, valorProduto, statusProduto) VALUES ('$fotoProduto', '$nomeProduto', '$descricaoProduto', '$valorProduto', 'disponivel')";
+                    $inserirProduto = "INSERT INTO Produtos (fotoProjeto, nomeProjeto, descricaoProjeto, valorProjeto, status_projeto) VALUES ('$fotoProjeto', '$nomeProjeto', '$descricaoProjeto', '$valorProjeto', 'disponivel')";
 
                     //Inclui o arquivo de conexão com o Banco de Dados
                     include("conexaoBD.php");
@@ -95,20 +95,20 @@
                         echo "
                             <div class='container mt-3'>
                                 <div class='container mt-3 text-center'>
-                                    <img src='$fotoProduto' style='width:150px;' title='Foto de $nomeProduto'>
+                                    <img src='$fotoProjeto' style='width:150px;' title='Foto de $nomeProjeto'>
                                 </div>
                                 <table class='table'>
                                     <tr>
                                         <th>NOME</th>
-                                        <td>$nomeProduto</td>
+                                        <td>$nomeProjeto</td>
                                     </tr>
                                     <tr>
                                         <th>DESCRIÇÃO DO PRODUTO</th>
-                                        <td>$descricaoProduto</td>
+                                        <td>$descricaoProjeto</td>
                                     </tr>
                                     <tr>
                                         <th>VALOR DO PRODUTO</th>
-                                        <td>$valorProduto</td>
+                                        <td>$valorProjeto</td>
                                     </tr>
                                 </table>
                             </div>

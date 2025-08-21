df324b8bcf9ad0c5c4c6b5eaa99fa2e0ed291853
alon <camiloalon07@gmail.com>
2025-08-21 17:40:42 -0300

    footer trocado

--- footer.php
+++ footer.php
@@ -2,9 +2,27 @@
         </div>
 
         <!-- Container para abrigar o rodapé do sistema -->
-        <div class="container-fluid text-center bg-dark text-light p-2 fixed-bottom">
-            Sistema desenvolvido nas aulas de Programação Web I do IFPR Telêmaco Borba
-        </div>  
+       <div class="container-fluid text-center bg-dark text-light p-2 fixed-bottom">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto">
+                        <div class="small m-0 text-white">
+                        Instituto Federal Campus Telêmaco Borba 
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <span class="text-white mx-1">&middot;</span>
+                        Alon
+                        <span class="text-white mx-1">&middot;</span>
+                        Diego
+                        <span class="text-white mx-1">&middot;</span>
+                        Gustavo
+                        <span class="text-white mx-1">&middot;</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
 
     </body>
 </html>

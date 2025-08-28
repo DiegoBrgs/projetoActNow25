325f4918ca09fa8842f6e136fce65fe889bffb78
alon <camiloalon07@gmail.com>
2025-08-28 15:20:22 -0300

    update visualizar projeto

--- visualizarProjeto.php
+++ visualizarProjeto.php
@@ -30,124 +30,89 @@
 
                         ?>
 
-                        <div class="d-flex justify-content-center mb-3">
-
-                            <div class="card" style="width:30%; border-style:none;">
-                                            
-                                <!-- Carousel -->
-                                <div id="Projeto" class="carousel slide" data-bs-ride="carousel" >
-
-                                    <!-- Indicators/dots -->
-                                    <div class="carousel-indicators">
-                                        <button type="button" data-bs-target="#Projeto" data-bs-slide-to="0" class="active"></button>
-                                        <button type="button" data-bs-target="#Projeto" data-bs-slide-to="1"></button>
-                                        <button type="button" data-bs-target="#Projeto" data-bs-slide-to="2"></button>
-                                    </div>
-
-                                    <!-- The slideshow/carousel -->
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <div class='position-relative'>
-                                                <?php
-                                                    if($status_projeto == 'esgotado'){
-                                                        echo "
-                                                            <div class='position-absolute top-50 start-50 translate-middle bg-danger text-white px-4 py-2 fs-6 fw-bold rounded shadow' style='z-index: 10; opacity: 0.85;'>
-                                                                ESGOTADO
-                                                            </div>
-                                                        ";
-                                                    }
-                                                    echo "
-                                                        <img class='d-block w-100' src='$fotoProjeto' alt='Foto de $nomeProjeto' ";
-                                                            if($status_projeto == 'esgotado'){
-                                                                echo "style='filter:grayscale(100%)' ";
-                                                            }
-                                                        echo ">";
-                                                ?>
-                                            </div>
-                                        </div>
-                                        <div class="carousel-item">
-                                            <div class='position-relative'>
-                                                <?php
-                                                    if($status_projeto == 'esgotado'){
-                                                        echo "
-                                                            <div class='position-absolute top-50 start-50 translate-middle bg-danger text-white px-4 py-2 fs-6 fw-bold rounded shadow' style='z-index: 10; opacity: 0.85;'>
-                                                                ESGOTADO
-                                                            </div>
-                                                        ";
-                                                    }
-                                                    echo "
-                                                        <img class='d-block w-100' src='$fotoProjeto' alt='Foto de $nomeProjeto' ";
-                                                            if($status_projeto == 'esgotado'){
-                                                                echo "style='filter:grayscale(100%)' ";
-                                                            }
-                                                        echo ">";
-                                                ?>
-                                            </div>
-                                        </div>
-                                        <div class="carousel-item">
-                                            <div class='position-relative'>
-                                                <?php
-                                                    if($status_projeto == 'esgotado'){
-                                                        echo "
-                                                            <div class='position-absolute top-50 start-50 translate-middle bg-danger text-white px-4 py-2 fs-6 fw-bold rounded shadow' style='z-index: 10; opacity: 0.85;'>
-                                                                ESGOTADO
-                                                            </div>
-                                                        ";
-                                                    }
-                                                    echo "
-                                                        <img class='d-block w-100' src='$fotoProjeto' alt='Foto de $nomeProjeto' ";
-                                                            if($status_projeto == 'esgotado'){
-                                                                echo "style='filter:grayscale(100%)' ";
-                                                            }
-                                                        echo ">";
-                                                ?>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <!-- Left and right controls/icons -->
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#Projeto" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon"></span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#Projeto" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon"></span>
-                                    </button>
-                                </div>
-                                
-                                <div class="card-body">
-                                    <h4 class="card-title"><b><?php echo $nomeProjeto ?></b></h4>
-                                    <p class="card-text">Vagas: <b> <?php echo $vagas ?></b></p>
-                                    <div class="card bg-light">
-                                        <div class="card-body">
+                        <div class="container my-4">
+                            <div class="row mx-auto shadow rounded overflow-hidden" style="max-width: 900px;">
+                                <!-- Coluna do carrossel -->
+                                <div class="col-md-6 p-0">
+                                    <div id="Projeto<?php echo $id_projeto ?>" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
                                             <?php
-                                                if($status_projeto != 'esgotado'){
+                                            for ($i = 0; $i < 3; $i++) {
+                                                $active = $i === 0 ? "active" : "";
+                                                echo "
+                                                <div class='carousel-item $active'>
+                                                    <div class='position-relative'>
+                                                ";
+                                                if ($status_projeto == 'esgotado') {
                                                     echo "
-                                                        <a href='#' title='Ingressar'>
-                                                            <button class='btn btn-outline-success'>
-                                                                <i class='bi bi-pin-angle' style='font-size:10pt;'></i>
-                                                                Ingressar em $nomeProjeto
-                                                            </button>
-                                                        </a>
-                                                    ";
-                                                }
-                                                else{
-                                                    echo "
-                                                        <div class='alert alert-secondary'>
-                                                            Projeto Esgotado! <i class='bi bi-emoji-frown'></i>
+                                                        <div class='position-absolute top-50 start-50 translate-middle bg-danger text-white px-3 py-1 fs-6 fw-bold rounded shadow' style='z-index: 10; opacity: 0.85;'>
+                                                            ESGOTADO
                                                         </div>
                                                     ";
                                                 }
-                                                echo "<br><br>
-                                                <b>$descricao</b>"
+                                                echo "
+                                                        <img class='d-block w-100 project-img' src='$fotoProjeto' alt='Foto de $nomeProjeto'";
+                                                if ($status_projeto == 'esgotado') {
+                                                    echo " style='filter: grayscale(100%)'";
+                                                }
+                                                echo ">
+                                                    </div>
+                                                </div>";
+                                            }
                                             ?>
                                         </div>
-                                        <br>
+
+                                        <!-- Controles -->
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#Projeto<?php echo $id_projeto ?>" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon"></span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#Projeto<?php echo $id_projeto ?>" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon"></span>
+                                        </button>
                                     </div>
                                 </div>
 
-                            </div>
+                                <!-- Coluna das informações -->
+                                <div class="col-md-6 bg-light p-3 d-flex flex-column justify-content-center shadow-sm">
+                                    <h5 class="card-title"><b><?php echo $nomeProjeto ?></b></h5>
+                                    <p class="card-text mb-9">Vagas: <b><?php echo $vagas ?></b></p>
 
+                                        
+                                    <div class='align-items-center'>
+                                            <img class='rounded-circle me-3 img-fluid w-35' src='$fotoEmpresa' alt='Foto da $nome_empresa' />
+                                                
+                                    </div>
+                                    <div class='small'>
+                                                <div class='fw-bold'>$nome_empresa</div>
+                                                <div class='text-muted'>$area_atuacao <br>Número de Vagas: <b>$vagas</b></div>
+                                            </div>
+                                    <br><br><br><br>
+                                    
+
+                                    <?php
+                                    if ($status_projeto != 'esgotado') {
+                                        echo "
+                                            <a href='#' title='Ingressar' class='mb-2'>
+                                                <button class='btn btn-outline-success btn-sm'>
+                                                    <i class='bi bi-pin-angle' style='font-size:10pt;'></i>
+                                                    Ingressar em $nomeProjeto
+                                                </button>
+                                            </a>
+                                        ";
+                                    } else {
+                                        echo "
+                                            <div class='alert alert-secondary py-1 px-2'>
+                                                Projeto Esgotado! <i class='bi bi-emoji-frown'></i>
+                                            </div>
+                                        ";
+                                    }
+
+                                    echo "<hr><p class='small'><b>$descricao</b></p>";
+                                    ?>
+                                </div>
+                            </div>
                         </div>
+
 
                     <?php
 
